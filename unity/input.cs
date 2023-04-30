26956c210bac5ea995b188cd2f8866e02de0a14d
Jocker <jocker@jockerMacBookPro.local>
2023-04-30 16:14:09 +0800

    rotation with mouse

--- unity/input.cs
+++ unity/input.cs
@@ -6,18 +6,21 @@ public class CharInput : MonoBehaviour {
     /*
         Добавить CharacterController в компоненты объекта
         Добавить CapsuleCollider в компоненты объекта
-        ?Добавить RigidBody в компоненты объекта
         добавить скрипт объекту
     */
 
     CharacterController charController; // Компонент контроллера перемещения
     float moveSpeed = 5.0f; // скорость
-    float horizontalInput; // величина перемещения по горизонтали
+    float horizontalInput; // перемещение по x
+    float verticalInput; // перемещение по z
     Vector3 movement; // величина перемещения
 
     float jumpHeight = 0.5f;
     float gravity = 0.045f;
     bool isJump = false; // начат прыжок
+
+    [SerializeField]
+    bool isRotateWithMouse = false;
 
     void Start() {
         charController = GetComponent<CharacterController>();
@@ -28,13 +31,15 @@ public class CharInput : MonoBehaviour {
 
     void Update() {
         // Настройки Input: Edit - Project Settings - InputManager
-        horizontalInput = Input.GetAxis("Horizontal");
+        horizontalInput = Input.GetAxis("Horizontal"); // for 2d
+        verticalInput = Input.GetAxis("Vertical");
         checkJumpStarted();
     }
 
     private void FixedUpdate() {
         if (charController) {
             move();
+            rotate();
             jump();
         }
     }
@@ -47,7 +52,8 @@ public class CharInput : MonoBehaviour {
     }
 
     private void move() {
-        movement.x = horizontalInput * moveSpeed * Time.deltaTime;
+        movement.x = horizontalInput * moveSpeed * Time.deltaTime; // 2d
+        movement.z = verticalInput * moveSpeed * Time.deltaTime;
         charController.Move(movement);
     }
 
@@ -63,5 +69,37 @@ public class CharInput : MonoBehaviour {
             isJump = false;
             movement.y = jumpHeight;
         }
+    }
+
+    private void rotate() {
+        if (isRotateWithMouse) {
+            rotateWithMouse();
+        } else {
+            rotateWithKey();
+        }
+
+    }
+
+    private void rotateWithKey() {
+        /* Поворот персонажа в сторону движения по кнопке
+        */
+        Vector3 directionInput = new Vector3(horizontalInput, 0, verticalInput);
+        if (directionInput != Vector3.zero) {
+            // Если лицо направлено в сторону z
+            //transform.forward = directionInput.normalized;
+            // Если мы разместили лицо справа (по x)
+            transform.forward = Quaternion.Euler(0, -90, 0) * directionInput.normalized;
+        }
+    }
+
+    private void rotateWithMouse() {
+        /* Поворот персонажа в сторону курсора мыши
+           глаза должны быть направлены в x сторону
+        */
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerPos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 direction = mousePos - playerPos;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(-angle, Vector3.up);
     }
 }
